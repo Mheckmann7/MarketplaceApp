9c3c1f835e8a4c9ba0bad433c4ef14fdfd0a385d
Mira Heckmann <miraheckmann@gmail.com>
2021-02-26 14:32:35 -0800

    adds front page

--- App.js
+++ App.js
@@ -1,18 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image, SafeAreaView, TouchableWithoutFeedback, TouchableOpacity} from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, SafeAreaView} from 'react-native';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Hello World!</Text>
-      <TouchableOpacity onPress={() => console.log("Touched")}>
-      <Image source={{
-        width: 200,
-        height: 300,
-        uri: "https://picsum.photos/200/300"
-        }} />
-      </TouchableOpacity>
+      <View style={styles.wineContainer}>
+        <ImageBackground
+          source={require('./assets/images/red-wine.jpg')}
+          style={styles.image} /> 
+        <View style={styles.titles}>
+          <Text style={styles.title}>Red Wines</Text>
+          <Text style={styles.subtitle}>Starting at $60</Text>
+        </View>
+
+      </View>
+      <StatusBar style="auto" /> 
     </SafeAreaView>
   );
 }
@@ -20,8 +23,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#461513',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  wineContainer: {
+    width: '100%',
+    height: '100%', 
+  },
+  titles: {
+    marginTop: '20%',
+    width: '100%',
+    alignItems: 'center', 
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: '500',
+  }, 
+  subtitle: {
+    fontSize: 16, 
+    color: '#461513',
+  }, 
+  image: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+    position: 'absolute'
+  }
 });
+
